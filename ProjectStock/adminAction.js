fc853a74cef43c35196368bde7c501516633a878
mxls34 <natakongame@gmail.com>
2026-02-12 14:43:39 +0700

    create Edit Product

--- ProjectStock/adminAction.js
+++ ProjectStock/adminAction.js
@@ -1,17 +1,31 @@
-let Stock = []
+let Stock = [];
+let htmlEdit = ``;
 
-function addStock(nameIngredient, numberIngredient) {
+function addStock() {
+    let idIngredient = `S${Stock.length}`;
+    let nameIngredient = document.getElementById('nameIngredient').value;
+    let numberIngredient = document.getElementById('numberIngredient').value;
 
+    let formatTime = new Date();
+    
     let products = {
+        ID: idIngredient,
         name: nameIngredient,
-        number: numberIngredient
+        number: numberIngredient,
+        day: `${formatTime.getDate()}/${formatTime.getMonth()}/${formatTime.getFullYear()} เวลา ${formatTime.getHours()}:${formatTime.getMinutes()}`
     }
-
-    Stock.push(products)
+    Stock.push(products);
+    
+    alert('เพิ่มข้อมูลเสร็จสิ้น');
+    
+    // clear input
+    document.getElementById('nameIngredient').value = '';
+    document.getElementById('numberIngredient').value = '';
 }
 
 function back() {
     document.getElementById('addContainer').style.display = "none";
+    document.getElementById('editContainer').style.display = "none";
 
     document.getElementById('container').style.display = "block";
 }
@@ -25,10 +39,27 @@ function checkSelect() {
         document.getElementById('container').style.display = "none";
         document.getElementById('addContainer').style.display = "block";
 
-        let nameIngredient = document.getElementById('').value;
-        let numberIngredient = document.getElementById('').value;
+    } else if (typeSelect == "edit") {
+        document.getElementById('container').style.display = "none";
+        document.getElementById('editContainer').style.display = "block";
 
-        addStock(nameIngredient, numberIngredient);
+        htmlEdit = '';  // รีเซ็ตเพื่อไม่ให้ซ้ำ
+        for (let i = 0; i < Stock.length; i++) {
+            htmlEdit += `<tr>`;
+            htmlEdit += `<td>${Stock[i].ID}</td>`;
+            htmlEdit += `<td>${Stock[i].name}</td>`;
+            htmlEdit += `<td>${Stock[i].number}</td>`;
+            htmlEdit += `<td>${Stock[i].day}</td>`;
+
+            htmlEdit += `<td>
+                            <button class="editBtn1" onclick="">แก้ไข</button>                       
+                            <button class="editBtn2" onclick="">ลบ</button>                       
+                        </td>`
+            htmlEdit += `</tr>`;
+        }
+        // ใส่ข้อมูลลงใน DOM
+        document.getElementById('showEdit').innerHTML = htmlEdit;
+
     }
 
 }
